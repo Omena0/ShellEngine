10b6e955af3bf76b08ea0861b6cad4e6277dc4a0
Omena0 <Omena0MC@gmail.com>
2023-08-30 23:26:38 +0300

    V1

---
+++ flappy.py
@@ -0,0 +1,97 @@
+from shellEngine import Game, Sprite, screen_height, screen_width, colors
+
+import os
+import sys
+import random as r
+import keyboard as kb
+import time as t
+from threading import Thread
+
+# Flappy bird example using shellEngine
+# Example by Omena0
+
+game = Game()
+
+def gameloop():
+    loop = 0
+    while True:
+        player.sety(1)
+        
+        for pillar in pillars:
+            pillar.setx(-1)
+
+            if player.x in pillar.xrange() and player.y in pillar.yrange():
+                if player.y in range(pillar.y+22,pillar.y+27): pass
+                else:
+                    game.screen = '\n'*screen_height
+                    sys.tracebacklimit = -100
+                    print('!!! You died! Killing program.. !!!'+' '*100)
+                    while True:
+                        os.kill(0,0)
+                    
+                    
+
+            if pillar.x < -5:
+                pillar.x = screen_width
+                pillar.y = r.randrange(10)-20
+        
+        t.sleep(0.16)
+        loop += 1
+
+def on_press(char:kb.KeyboardEvent):
+    char = char.name
+    a.changed = True
+    if char == 'w':
+        Thread(target=jump,daemon=True).start()
+
+def jump():
+    for i in range(round(screen_height/4)):
+        player.sety(-1)
+        t.sleep(0.15*(i/5))
+    
+print('\n'*50)
+
+a = Game()
+
+player_texture = [
+    colors[3]*3,
+    colors[3]*3
+]
+
+pillar_texture = []
+
+for i in range(round(screen_height)):
+    pillar_texture.append(colors[1]+colors[3]*4+colors[1])
+    
+pillar_texture.append(colors[4]*6)
+for i in range(round(screen_height/4)+1):
+    pillar_texture.append(colors[1]*6)
+pillar_texture.append(colors[4]*6)
+
+for i in range(round(screen_height)):
+    pillar_texture.append(colors[1]+colors[3]*4+colors[1])
+
+
+pillars = []
+
+for i in range(3):
+    if i == 0: continue
+    pillar = Sprite(pillar_texture)
+
+    pillar.x = 40*i
+    pillar.y = r.randrange(10)-20
+    pillar.wall_physics = False
+    pillars.append(pillar)
+
+player = Sprite(player_texture)
+
+player.x = 15
+
+kb.on_press(on_press)
+
+Thread(target=gameloop,daemon=True).start()
+
+a.run()
+
+
+    
---
+++ shellEngine.py
@@ -0,0 +1,132 @@
+import time as t
+from threading import Thread
+
+screen_height = 20
+screen_width = 100
+
+colors = [' ','░','▒','▓','█']
+
+cap_fps = False
+
+sprites = []
+
+def back(num:int=1):return f'\x1b[{num}A'
+
+class Sprite:
+    def __init__(self,texture:list):
+        self.x = 2
+        self.y = 2
+        self.width   = len(texture[0])
+        self.height  = len(texture)
+        self.texture = texture
+        self.wall_physics = True
+        sprites.append(self)
+        
+    def sety(self,y):
+        if self.wall_physics:
+            if self.y+y > screen_height or self.y+y+self.height > screen_height or self.y+y < 0:
+                return
+        self.y += y
+        game.changed = True
+        
+    def setx(self,x):
+        if self.wall_physics:
+            if self.x+x > screen_width or self.x+x+self.width > screen_width or self.x+x < 0:
+                return
+        self.x += x
+        game.changed = True
+        
+    def xrange(self):
+        return range(self.x,self.x+self.width)
+    
+    def yrange(self):
+        return range(self.y,self.y+self.height)
+        
+class Game:
+    def __init__(self):
+        global game
+        game = self
+        self.screen = ''
+        self.frame = 0
+        self.tick=0
+        self.fps = 0
+        self.fps_per_sec = 0
+        self.fps_list = []
+        self.tick_time = 0
+        self.mspt = 0
+        self.mspt_list = []
+        self.changed = True
+        self._init_display()
+        
+    def _init_display(self):
+        self.screen = ''
+        for i in range(screen_height):
+            for i in range(screen_width):
+                self.screen += ''
+            self.screen += '\n'
+    
+    def screen_renderer(self):
+        self.frame += 1
+        self.fps_per_sec += 1
+        if not self.changed: return
+        self.changed = False
+        self.screen = ''
+        for y in range(screen_height):
+            for x in range(screen_width):
+                # Rendering code and shit
+                color = self.render(x,y)
+                self.screen += color
+            self.screen += '\n'
+        
+    def render(self,x,y):
+        color = colors[1]
+        for sprite in sprites:
+            if x in sprite.xrange():
+                if y in sprite.yrange():
+                    try:
+                        color = sprite.texture[y-sprite.y-1][x-sprite.x-1]
+                    except: pass
+        return color
+    
+    def update_display(self):
+        print(back(screen_height+10),end='')
+        print(f'FPS: {self.fps:2<} TICK: {self.tick} FRAME: {self.frame} TICK TIME: {self.mspt}')
+        print(self.screen,end='')
+        self.tick += 1
+        
+    def run(self):
+        global screen_height
+        render = True
+        Thread(target=self.loop,daemon=True).start()
+        while True:
+            start = t.time()
+            self.update_display()
+            self.screen_renderer()
+            end = t.time()
+            # tick time calc
+            duration = (end - start)
+            self.tick_time = duration
+            self.mspt_list.append(duration)
+            if len(self.mspt_list) > 25: self.mspt_list.pop(1)
+            self.mspt = sum(self.mspt_list)/len(self.mspt_list)
+            try:
+                if cap_fps: t.sleep(0.01 - duration)
+            except: pass
+            
+    def loop(self):
+        loop = 0
+        while True:
+            start = t.time()
+
+            self.fps_list.append(self.fps_per_sec)
+            self.fps_per_sec = 0
+            self.fps = round(sum(self.fps_list)/len(self.fps_list),2)
+            if len(self.fps_list) > 25: self.fps_list.pop(1)
+            
+            end = t.time()
+            t.sleep(0.1-(end-start))
+            loop += 1
+
+
+
+    
